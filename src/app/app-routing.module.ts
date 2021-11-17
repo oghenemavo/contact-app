6786f5ea400fc4142e688856434332f850b57faf
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2021-11-17 11:25:56 +0100

    changes v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientFormComponent } from './views/components/clients/client-form/client-form.component';
+import { SearchFormComponent } from './views/components/search/search-form/search-form.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'clients',
     component: ClientFormComponent
-  }
+  },
+  {
+    path: 'search',
+    component: SearchFormComponent
+  },
+  {
+    path: '',
+    redirectTo: "/clients",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
